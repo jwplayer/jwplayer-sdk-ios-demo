6ab8da79f3feafe901aedcf7618b3b8679f38c18
Daniel Berger <doniberger@gmail.com>
2018-02-01 14:25:54 -0500

    dynamic_library_compatible

--- JWPlayer-Developer-Demo/JWPlayer-Developer-Demo-Bridging-Header.h
+++ JWPlayer-Developer-Demo/JWPlayer-Developer-Demo-Bridging-Header.h
@@ -2,4 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <JWPlayer-iOS-SDK/JWPlayerController.h>
+#import <JWPlayer_iOS_SDK/JWPlayerController.h>
